d1cf4b07157190241f7ec18866aec1041e23b90c
Chinmay <chinz.sandur07@gmail.com>
2024-02-29 16:41:25 +0530

    add useEffect variations

--- dummy/src/App.js
+++ dummy/src/App.js
@@ -4,14 +4,42 @@ import './App.css';
 function App() {
 
   const [text, setText] = useState("");
+  const [name, setName] = useState("Chinz");
 
+  //it will execute after every "render"
+  //VARIATION-1
+  // useEffect(() => {
+  //   console.log("UI rendering Done");
+  // })
+
+  //Variation-2 -- this useEffect will run only for once when the App component will run for the first time
+
+  // THIS is because we are using a dependency array here.
+  // useEffect(() => {
+  //   console.log("UI RENDERING DONE");
+  // }, []);
+
+  //variation - 3
+  // this will run after every time the dependency changes
+
+  // useEffect(() => {
+  //   console.log("Change Observed");
+  // }, [name]);
+
+
+  //Variation-4
   useEffect(() => {
-    console.log("UI rendering Done");
-  })
+    console.log("listener added");
+    return () => {
+      console.log("listener removed");
+    }
+  }, [text]);
 
+  
   function changeHandler(e) {
     console.log(text);
     setText(e.target.value);
+    // setName(e.target.value)
   }
 
   return (
